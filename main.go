da8054a447ea5b7df7cbee939c70777f023037c5
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2011-04-15 11:54:37 -0400

    Command-line args.

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"flag"
+	"os"
 )
 
-const (
-	pageName = "Jersey City"
-	lang     = "en"
-)
+const usage = `usage: wptrans LANG NAME
+where LANG is a 2- or 3-letter Wikipedia language code
+and NAME is the name of a page on the LANG Wikipedia`
 
 func main() {
-	d := NewDict(lang, pageName)
-	fmt.Println(d)
+	flag.Parse()
+	if flag.NArg() != 2 {
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(1)
+	}
+	fmt.Println(NewDict(flag.Arg(0), flag.Arg(1)))
 }
